aa6ad35d965036a0274c852b7d61a7f13b97cfc1
matsutake0904 <62604920+matsutake0904@users.noreply.github.com>
2021-01-30 10:14:07 +0900

    Update settings.py

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME' : 'myproject',
         'USER': 'ryo',
-        'PASSWORD': 'maryo1994',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
